932c581ad62cfa04d366d83512f4477d6b42d866
Chris Barker <PythonCHB@gmail.com>
2020-10-25 12:04:13 -0700

    fixed bug in auto_test.py

--- auto_test.py
+++ auto_test.py
@@ -1,4 +1,4 @@
-from series import fibonacci,lucas, sum_series
+from series import fibonacci, lucas, sum_series
 
 
 assert fibonacci(-1) is None
@@ -16,10 +16,10 @@ assert lucas(3) == 4
 
 assert sum_series(0) == fibonacci(0)
 assert sum_series(1) == fibonacci(1)
-assert sum_series(3) == fibonacci(2)
+assert sum_series(3) == fibonacci(3)
 
 assert sum_series(0, 2, 1) == lucas(0)
 assert sum_series(1, 2, 1) == lucas(1)
-assert sum_series(3, 2, 1) == lucas(2)
+assert sum_series(3, 2, 1) == lucas(3)
 
 assert sum_series(3, 3, 2) == 7
